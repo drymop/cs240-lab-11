818cbbdfed334812b400875a039ec3f2de10140b
jo6kg <jo6kg@mathcs04.richmond.edu>
2018-04-15 13:54:00 -0400

    Wrote tester

--- tester.cpp
+++ tester.cpp
@@ -10,9 +10,9 @@ int main(int argc, char const *argv[])
 
 	//Adds the value 5 to the LinkedList then checks to see if the size and get methods call the value
         l.add(5);
-	std::cout << "Expected Size: 0" << std::endl << "Actual: " << l.size() << std::endl;
+	std::cout << "Expected Size: 1" << std::endl << "Actual: " << l.size() << std::endl;
 	std::cout << "Expected First Value: 5" << std::endl 
-		  << "Actual First Value: " << l.get(0);
+		  << "Actual First Value: " << l.get(0) << endl;
 
 	//Tests remove then after the value is removed test to see the LinkedList is empty
 	l.remove(0);
